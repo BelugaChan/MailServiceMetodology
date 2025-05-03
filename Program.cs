3ca49b1065c2dbd2e122ab3f627ae723cdfd9052
BelugaChan <tuchnyy@list.ru>
2025-05-03 11:18:40 +0300

    Register new services in DI container
    Signed-off-by: BelugaChan <tuchnyy@list.ru>

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using MailServiceMetodology.Models;
 using MailServiceMetodology.Services;
 using RabbitMqListener.Interfaces;
 using RabbitMqModel.Models;
+using Microsoft.AspNetCore.Hosting;
 
 namespace MailServiceMetodology
 {
@@ -13,17 +14,18 @@ namespace MailServiceMetodology
         {
             var builder = Host.CreateApplicationBuilder(args);
 
+
             builder.Services.Configure<SmtpOptions>(builder.Configuration.GetSection(nameof(SmtpOptions)));
             builder.Services.Configure<RabbitMqOptions>(builder.Configuration.GetSection(nameof(RabbitMqOptions)));
-            builder.Services.AddSingleton<IEmailServiceBase<RabbitMqTwoFaConsumer, string>, TwoFaEmailService>();
+            builder.Services.AddSingleton<IEmailServiceBase<RabbitMqTwoFaConsumer, string>, VerificationCodeEmailService>();
             builder.Services.AddSingleton<IEmailServiceBase<RabbitMqResetPasswordConsumer,string>,ResetPasswordEmailService>();
 
             builder.Services
                 .AddHostedService<TwoFaRabbitListener>()
-                .AddSingleton<IRabbitMqListenerBase<RabbitMqTwoFaConsumer>,TwoFaRabbitListener>();
+                .AddSingleton<IRabbitMqListenerBase,TwoFaRabbitListener>();
             builder.Services
                 .AddHostedService<ResetPasswordRabbitListener>()
-                .AddSingleton<IRabbitMqListenerBase<RabbitMqResetPasswordConsumer>, ResetPasswordRabbitListener>();
+                .AddSingleton<IRabbitMqListenerBase, ResetPasswordRabbitListener>();
            
             var host = builder.Build();
 

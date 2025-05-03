c729b4f711290ce5709a95ae8f1c94c6869c37ab
BelugaChan <tuchnyy@list.ru>
2025-05-03 11:16:26 +0300

    Add new option class for smtp
    Signed-off-by: BelugaChan <tuchnyy@list.ru>

---
+++ Models/SmtpOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace MailServiceMetodology.Models
+{
+    public class SmtpOptions
+    {
+        public string From { get; set; }
+
+        public string Server { get; set; }
+
+        public int Port { get; set; }
+
+        public string Password { get; set; }
+    }
+}
